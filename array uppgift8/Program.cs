6457060619413c8a52f3fd6305ea4571e12a157f
Emad Iddin Ali <ea224pn@student.lnu.se>
2024-06-05 19:15:05 +0200

    Add project files.

---
+++ array uppgift8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inlamning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = { 1234, 5678, 9101, 1121, 3141 };
+            foreach (int t in tal)
+            {
+                string strTal = t.ToString();
+                foreach (char c in strTal)
+                {
+                    Console.WriteLine(c);
+                    Thread.Sleep(500);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
